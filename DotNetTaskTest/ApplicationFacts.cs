fe7fc56cfa9bad8f926f06bcddb9432ddfb63b8c
Israel Oluwafemi <israeloluwafemi44@gmail.com>
2024-06-20 00:56:26 +0100

    added unit test

--- DotNetTaskTest/ApplicationFacts.cs
+++ DotNetTaskTest/ApplicationFacts.cs
@@ -1,6 +1,37 @@
+using AutoFixture;
+using DotNetTask.Core;
+using DotNetTask.Data.DTOs;
+using DotNetTask.Data.Models;
+using DotNetTask.Repository;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+
 namespace DotNetTaskTest;
 
 public class ApplicationFacts
 {
-    
+        private IApplicationService applicationService;
+    private readonly IFixture fixture;
+    public ApplicationFacts()
+    {
+        fixture = new Fixture();
+    }
+    [Fact]
+    public async void ShouldSubmitApplication()
+    {
+        // Arrange
+        var applicationDto = fixture.Create<ApplicationDTO>();
+        var application = fixture.Build<Application>().With(x => x.PersonalInformation, applicationDto.PersonalInformation)
+            .With(x => x.AdditionalQuestions, applicationDto.AdditionalQuestions).Create();
+        var applicationRepo = Substitute.For<IApplicationRepository>();
+        applicationRepo.CreateApplicationAsync(Arg.Any<Application>()).Returns(application);
+        applicationService = new ApplicationService(applicationRepo);
+        
+        //Act
+        var response = await applicationService.CreateApplicationAsync(applicationDto);
+
+        //Assert
+        Assert.Equal(StatusCodes.Status201Created, response.StatusCode);
+
+    }
 }
--- DotNetTaskTest/ProgramFacts.cs
+++ DotNetTaskTest/ProgramFacts.cs
@@ -1,6 +1,92 @@
+using AutoFixture;
+using DotNetTask.Core;
+using DotNetTask.Data.DTOs;
+using DotNetTask.Data.Models;
+using DotNetTask.Repository;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+
 namespace DotNetTaskTest;
 
 public class ProgramFacts
 {
+    private IProgramService programService;
+    private readonly IFixture fixture;
+    public ProgramFacts()
+    {
+        fixture = new Fixture();
+    }
+    [Fact]
+    public async void ShouldCreateProgram()
+    {
+        // Arrange
+        var programDto = fixture.Create<ProgramFormDTO>();
+        var programForm = fixture.Build<ProgramForm>().With(x => x.ProgramDescription, programDto.ProgramTitle)
+            .With(x => x.ProgramDescription, programDto.ProgramDescription)
+            .With(x => x.AdditionalQuestions, programDto.AdditionalQuestions).Create();
+        var programRepo = Substitute.For<IProgramRepository>();
+        programRepo.CreateProgramAsync(Arg.Any<ProgramForm>()).Returns(programForm);
+        programService = new ProgramService(programRepo);
+        
+        //Act
+        var response = await programService.CreateProgramAsync(programDto);
+
+        //Assert
+        Assert.Equal(StatusCodes.Status201Created, response.StatusCode);
+
+    }
+    [Fact]
+    public async void ShouldDeleteProgram()
+    {
+        // Arrange
+        var program = fixture.Create<ProgramForm>();
+        var programRepo = Substitute.For<IProgramRepository>();
+        programRepo.GetProgramByIdAsync(program.Id).Returns(program);
+        //programRepo.DeleteProgramAsync(programForm.Id).Returns(programForm);
+        programService = new ProgramService(programRepo);
+        
+        //Act
+        var response = await programService.DeleteProgramAsync(program.Id);
+
+        //Assert
+        Assert.Equal(StatusCodes.Status204NoContent, response.StatusCode);
+
+    }
     
+    [Fact]
+    public async void ShouldUpdateProgram()
+    {
+        // Arrange
+        var programOld = fixture.Create<ProgramForm>();
+        var programNew = fixture.Build<ProgramForm>().With(x => x.Id, programOld.Id).Create();
+        var programRepo = Substitute.For<IProgramRepository>();
+        programRepo.GetProgramByIdAsync(programNew.Id).Returns(programOld);
+        //programRepo.DeleteProgramAsync(programForm.Id).Returns(programForm);
+        programService = new ProgramService(programRepo);
+        
+        //Act
+        var response = await programService.UpdateProgramAsync(programNew);
+
+        //Assert
+        Assert.Equal(StatusCodes.Status204NoContent, response.StatusCode);
+
+    }
+    
+    [Fact]
+    public async void ShouldGetProgram()
+    {
+        // Arrange
+        var program = fixture.Create<ProgramForm>();
+        var programRepo = Substitute.For<IProgramRepository>();
+        programRepo.GetProgramByIdAsync(program.Id).Returns(program);
+        programService = new ProgramService(programRepo);
+        
+        //Act
+        var response = await programService.GetProgramByIdAsync(program.Id);
+
+        //Assert
+        Assert.Equal(StatusCodes.Status200OK, response.StatusCode);
+        Assert.Equal(program.Id, response.Data.Id);
+
+    }
 }
